7d76d1c0c9cfe5fccf3f8fbb7a0fd33a3ea65c95
Abdul Rahman <ar69king@gmail.com>
2025-04-18 02:00:06 +0500

    feat: restructure dashboard layout and integrate sidebar navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,56 @@
 
 import { DataTableDemo } from "@/components/example-employee-table";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="flex items-center justify-between w-full max-w-3xl mx-auto">
-        <div className="fixed top-4 right-4">
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/">
+                  Odoo Lens
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div className="flex-1" />
+          <div className="hidden md:block" />
           <ThemeToggle />
-        </div>
-      </header>
-      <main className="flex flex-col items-center justify-center gap-8">
-        {/* Employee Table Example */}
-        <div className="w-full max-w-3xl mx-auto">
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
           <DataTableDemo />
         </div>
-
-      </main>
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
